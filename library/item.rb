cf7f5f43bf27b10fe1fb99c2646f70c616189054
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-09 22:33:28 +0500

    Implement  labels properties and methods, including method overriding

--- library/item.rb
+++ library/item.rb
@@ -1,23 +1,32 @@
-require 'securerandom'
 require 'date'
 
 class Item
-  attr_accessor :genre, :author, :source, :label, :published_date, :archived
   attr_reader :id
+  attr_accessor :publish_date, :archived, :label
 
-  def initialize(published_date, id = SecureRandom.uuid)
-    @published_date = published_date
-    @id = id
-    @archived = false
+  def initialize(publish_date, archived = false)
+    @id = Random.rand(1..1000)
+    @publish_date = Date.parse(publish_date)
+    @archived = archived
+  end 
+
+  def move_to_archive
+    if can_be_archived?
+      @archived = true
+    else
+      @archived = false
+    end
   end
 
-  # def move_to_archive
-  #   @archived = true if can_be_archived?
-  # end
+  def label=(label)
+    @label = label
+    label.items << self
+  end
 
-  # private
+  private
 
-  # def can_be_archived?
-  #   @published_date.to_date < Date.today.year - 10
-  # end
+  def can_be_archived?
+    Date.today.year - @publish_date.year > 10
+  end
+
 end
