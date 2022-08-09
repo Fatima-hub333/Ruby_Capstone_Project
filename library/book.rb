e027a8626f70a3cf9492a54a43a13e33f69227a9
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-09 22:31:57 +0500

    Implement Book properties and methods, including method overriding

--- library/book.rb
+++ library/book.rb
@@ -1,42 +1,16 @@
 require_relative 'item'
-require 'securerandom'
 
 class Book < Item
-  attr_reader :id
-  attr_accessor :cover_state, :publisher, :archived, :genre, :author, :label, :published_date
+  attr_accessor :publish_date, :publisher, :cover_state
 
-  def initialize(published_date, publisher, cover_state = 'good', id = SecureRandom.uuid, archived: false)
-    super(published_date, id, archived: archived)
-    @published_date = published_date
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
     @publisher = publisher
-    @id = id
-    @cover_state = cover_state
-    move_to_archive
+    @cover_state = cover_state.downcase
   end
-
-  def to_json(*args)
-    {
-      JSON.create_id => self.class.name,
-      'published_date' => @published_date.to_date,
-      'publisher' => @publisher,
-      'id' => @id,
-      'archived' => @archived,
-      'cover_state' => @cover_state
-    }.to_json(*args)
-  end
-
-  def self.json_create(book)
-    year, month, day = book['published_date'].split('-')
-    new(
-      Date.new(year.to_i, month.to_i, day.to_i),
-      book['publisher'], book['cover_state'],
-      book['id'], archived: book['archived']
-    )
-  end
-
-  private
 
   def can_be_archived?
-    @published_date.to_date.year < Date.today.year - 10 || @cover_state == 'bad'
-  end
+    Date.today.year - @publish_date.year > 10 || @cover_state == 'bad'
+  end 
+
 end
