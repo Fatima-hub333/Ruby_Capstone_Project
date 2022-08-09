73fff3aedcffacf5103c9f3b195826a54cfda9fc
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-09 22:35:57 +0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,35 @@
-require_relative 'app'
+require_relative './app'
 
 def main
   app = App.new
-  app.run
+  app.greeter
+  input = nil
+  while input != '8'
+    app.instructions
+    input = gets.chomp
+    # options(input, app)
+  end
+  puts
+  puts 'Thanks for using Quckify!!'
 end
+
+# def options(input, app)
+#   case input
+#   when '1'
+#     app.list_books
+#   when '2'
+#     app.list_music_albums
+#   when '3'
+#     app.list_labels
+#   when '4'
+#     app.list_authors
+#   when '5'
+#     app.list_sources
+#   when '6'
+#     app.add_book
+#   when '7'
+#     app.add_music_album
+#   end
+# end
 
 main
