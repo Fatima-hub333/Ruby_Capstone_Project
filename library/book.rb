53ffec0d939a831bd043655c139e7f173fe04d3c
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-09 20:12:10 +0500

    Update TDD

--- library/book.rb
+++ library/book.rb
@@ -1,6 +1,6 @@
 require_relative 'item'
 require_relative 'securerandom'
 
-class Book < Item
+class Book
   def initialize(published_date, id = SecureRandom.uuid)
 end
--- spec/unit/book_spec.rb
+++ spec/unit/book_spec.rb
@@ -4,6 +4,9 @@ require 'date'
 describe 'A book' do
   date = DateTime.new(2015, 12, 25)
   let(:book) { Book.new date, 'the-item-id' }
+
+  it 'should be an instance of the item' do
+    expect(book.is_a?(Item)).to be true
 end
 
   
