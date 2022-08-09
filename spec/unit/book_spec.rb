b8aa4d3ecb92185341f8c025f74ce48ef17aaa25
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-09 20:13:18 +0500

    Write full TDD for book class

--- spec/unit/book_spec.rb
+++ spec/unit/book_spec.rb
@@ -9,4 +9,16 @@ describe 'A book' do
     expect(book.is_a?(Item)).to be true
 end
 
+it 'should have a published date' do
+    expect(book.published_date).to eq date
+  end
+  it 'should have a id' do
+    expect(book.id).to eq 'the-item-id'
+  end
+  it 'should overwrite parent method if cover_state is bad' do
+    book.cover_state = 'bad'
+    expect(book.can_be_archived?).to be true
+  end
+end
+
   
