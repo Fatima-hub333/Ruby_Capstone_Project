58612dec810f1b3c941089a49b6d81777aa483aa
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-10 09:09:02 +0500

    Add options to CLI
    -List all books
    -List all labels
    -Add books

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
+require_relative './library/book'
+require_relative './library/label'
+
 class App
+  def initialize
+    @books = []
+    @labels = []
+  end
+
   def greeter
     print "Hello! Welcome to Quckify!\n\n"
   end
@@ -6,12 +14,46 @@ class App
   def instructions
     puts "Please choose an option by entering a number:
     1 - List all books
-    2 - List all music albums
-    3 - List all labels (e.g. 'Gift', 'New')
-    4 - List all authors (e.g. 'Stephen King')
-    5 - List all sources (e.g. 'From a friend', 'Online shop')
-    6 - Add a book
-    7 - Add a music album
-    8 - Exit"
+    2 - List all labels (e.g. 'Gift', 'New')
+    3 - Add a book
+    4 - Exit"
+    puts
+  end
+
+  def list_all_books
+    if @books.count.zero?
+      puts "There are not books registered. Please add one!\n\n"
+    end
+    @books.each do |item|
+      puts "[#{item.class}] Publisher: #{item.publisher} Published date: #{item.publish_date} Cover_state: #{item.cover_state} Label: #{item.label.title}\n\n"
+    end
+  end
+
+  def list_all_labels
+    if @labels.count.zero?
+      puts "There are not labels registered. Please add one!\n\n"
+    end
+    @labels.each do |item|
+      puts "[#{item.class}] Name: #{item.title} Color: #{item.color}\n\n"
+    end
+  end
+
+  def add_book
+    print 'Enter publish date: '
+    publish_date = gets.chomp
+    print 'Enter publisher: '
+    publisher = gets.chomp
+    print 'Whats the cover state of the book? '
+    cover_state = gets.chomp
+    book = Book.new(publish_date, publisher, cover_state)
+    print 'Enter a label for your item eg. Gift, New: '
+    label_title = gets.chomp
+    print 'Enter a color for your label: '
+    label_color = gets.chomp
+    label = Label.new(label_title, label_color)
+    @labels << label
+    book.label=label
+    @books << book
+    puts
   end
 end
--- main.rb
+++ main.rb
@@ -4,32 +4,24 @@ def main
   app = App.new
   app.greeter
   input = nil
-  while input != '8'
+  while input != '4'
     app.instructions
     input = gets.chomp
-    # options(input, app)
+    options(input, app)
   end
   puts
-  puts 'Thanks for using Quckify!!'
+  puts 'Thanks for using Stockify CLI!!'
 end
 
-# def options(input, app)
-#   case input
-#   when '1'
-#     app.list_books
-#   when '2'
-#     app.list_music_albums
-#   when '3'
-#     app.list_labels
-#   when '4'
-#     app.list_authors
-#   when '5'
-#     app.list_sources
-#   when '6'
-#     app.add_book
-#   when '7'
-#     app.add_music_album
-#   end
-# end
+def options(input, app)
+  case input
+  when '1'
+    app.list_all_books
+  when '2'
+    app.list_all_labels
+  when '3'
+    app.add_book
+  end
+end
 
 main
